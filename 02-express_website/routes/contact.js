59209bbbb6a0348f5d014d0c8f6469fd549de540
Ryan P.C. McQuen <ryan.q@linux.com>
2015-12-03 14:09:02 -0800

    nodemailer works!
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- 02-express_website/routes/contact.js
+++ 02-express_website/routes/contact.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+router.post('/send', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: process.env.NODEMAILER_SERVICE,
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS
+    }
+  });
+  var mailOptions = {
+    from: "Rybot <nodemailer@ryanpcmcquen.com>",
+    to: process.env.NODEMAILER_USER,
+    subject: 'Website submission',
+    text: 'You have a new submission with the following details ... Name: ' + req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+    html: '<p>You got a new submission with the following details ... </p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
