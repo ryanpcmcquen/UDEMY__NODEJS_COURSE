b5f662fd13d9d1a0db7c71f4e9667de7be5c4e2a
Ryan P.C. McQuen <ryan.q@linux.com>
2015-12-14 07:07:34 -0800

    i think it may work ...
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- 03-nodeauth/app.js
+++ 03-nodeauth/app.js
@@ -24,12 +24,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // handle file uploads
-app.use(multer({dest:'./uploads'}));
-/*
+//app.use(multer({dest:'./uploads'}));
+
 var uploads = multer({
   dest: './uploads'
 });
-*/
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
